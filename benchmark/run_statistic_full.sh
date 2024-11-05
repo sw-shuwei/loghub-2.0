0d3080dca25f0a4557ed7d7759c43e88f881a3e3
Shu-Wei Huang <N96114213@gs.ncku.edu.tw>
2024-11-05 17:12:00 -0500

    Add .gitignore

--- benchmark/run_statistic_full.sh
+++
@@ -1,9 +0,0 @@
-# source ~/.zshrc
-# conda activate logevaluate
-
-cd evaluation
-for technique in AEL Drain IPLoM LenMa LFA LogCluster LogMine Logram LogSig MoLFI SHISO SLCT Spell
-do
-    echo ${technique}
-    python ${technique}_eval.py -full
-done
